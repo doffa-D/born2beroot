dbf102877cbc6eb94fe6f8505ace5ac07ee2466e
doffy <54211553+doffa-D@users.noreply.github.com>
2022-11-26 11:06:02 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,32 +1,25 @@
-#!/bin/bash
-arc=$(uname -a)
-pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
-vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
-fram=$(free -m | awk '$1 == "Mem:" {print $2}')
-uram=$(free -m | awk '$1 == "Mem:" {print $3}')
-pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
-fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
-udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
-pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
-cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
-lvmt=$(lsblk | grep "lvm" | wc -l)
-lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
-#You need to install net tools for the next step [$ sudo apt install net-tools]
-ctcp=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
-ulog=$(users | wc -w)
-ip=$(hostname -I)
-mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
-cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l) # journalctl should be running as sudo but our script is running as root so we don't need in sudo here
-wall "	#Architecture: $arc
-	#CPU physical: $pcpu
-	#vCPU: $vcpu
-	#Memory Usage: $uram/${fram}MB ($pram%)
-	#Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
-	#CPU load: $cpul
-	#Last boot: $lb
-	#LVM use: $lvmu
-	#Connexions TCP: $ctcp ESTABLISHED
-	#User log: $ulog
-	#Network: IP $ip ($mac)
-	#Sudo: $cmds cmd" # broadcast our system information on all terminals
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    monitoring.sh                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: hdagdagu <hdagdagu@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/11/05 19:53:12 by hdagdagu          #+#    #+#              #
+#    Updated: 2022/11/06 17:59:23 by hdagdagu         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!bin/bash
+echo "	# Architecture	:	$(uname -a)
+	# CPU physical	:	$(nproc)
+	# vCPU			:	$(cat /proc/cpuinfo | grep processor | wc -l)
+	# Memory Usage	:	$(free -m | awk '$1 == "Mem:" {print $3"/"$2 "MB"}') $(free | awk '$1 == "Mem:" {printf("(%.1f"), $3/$2*100}') %)
+	# Disk Usage		:	$(df -Bg | grep "/dev/" | grep -v "/boot" | awk '{ud += $3} {fd += $2} END {print ud"/"fd "GB (" ud/fd*100 " %)"}')
+	# CPU load		:	$(top -bn1 | grep '^%Cpu' | awk '{printf("%.1f%%"), $2}')
+	# Last boot		:	$(who -b | awk '{print $3,$4}')
+	# LVM use			:	$(lsblk | grep "lvm" | awk '{if ($1) {print "yes";exit} else {print "no"}}')
+	# Connections TCP	:	$(cat /proc/net/sockstat{,6}| grep "TCP:" | awk '{printf $3" ESTABLISHED"}')
+	# User Log		:	$(users | wc -w)
+	# Network			:	IP $(hostname -I | awk '{print $1}') $(ip link | grep "link/ether" | awk '{print "("$2")"}')
+	# Sudo			:	$(journalctl _COMM=sudo | grep COMMAND | wc -l) cmd"
